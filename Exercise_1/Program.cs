a2d3301b5fa98e4fdd2fa4685e0065a6d0a770a1
Василий Виссаров <vissar2016@rambler.ru>
2022-10-24 19:36:36 +0300

    Ex_1 done

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,33 @@
+﻿bool PalindCheck(int number)
+{
+int n = number;
+int res = 0;
+
+while(n>0)
+{
+    int r = n % 10;
+    res = res * 10 + r;
+    n = n / 10;
+}
+if(number == res)
+{
+return true;  
+}
+else
+{
+    return false;
+}
+
+}
+Console.WriteLine("Input number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(PalindCheck(num))
+    {
+        Console.WriteLine("Its palindrome");
+    }
+else
+    {
+        Console.WriteLine("Isn't palindrome");
+    }
+
