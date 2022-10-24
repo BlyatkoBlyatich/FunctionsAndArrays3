d657d1ad077158ffbf6031cb5e06ececb265dd75
Василий Виссаров <vissar2016@rambler.ru>
2022-10-24 19:37:20 +0300

    Ex_3 done

---
+++ Exercise_3/Program.cs
@@ -0,0 +1,14 @@
+﻿void Table(int number)
+{
+    int i = 1;
+    while(i<=number)
+    {
+        double count = Math.Pow(i, 3);
+        Console.Write(count + " ");
+        i++;
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int res = Convert.ToInt32(Console.ReadLine());
+Table(res);
